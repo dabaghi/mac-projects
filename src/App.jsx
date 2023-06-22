1ca64cf566d4e8d1c18d158f648ad09820c96406
hameddabaghi <h.dabaghi@gmail.com>
2023-06-22 03:37:40 +0330

    add header component

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <div>
+          <h1>salam</h1>
+        </div>
+      </div>
+      <Header />
+      <footer />
+    </>
+  );
+}
+
+export default App;
